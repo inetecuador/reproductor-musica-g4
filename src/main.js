2da6b9e428f81079383c7cbecb0d196f1ed7f6ee
jager <inetecuador@gmail.com>
2024-02-21 22:07:21 -0500

    catalogo general

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 console.log('funcionando');
 
 class Song{
-    constructor(cover, nombre, autor, artista, duracion, album, anio, genero, urlSong) {
+    constructor(id, cover, nombre, autor, artista, duracion, album, anio, genero, url) {
+        this.id = id;
         this.cover = cover;
         this.nombre = nombre;
         this.autor = autor;
@@ -10,15 +11,14 @@ class Song{
         this.album = album;
         this.anio = anio;
         this.genero = genero;
-        this.urlSong = urlSong;
+        this.url = url;
     }
 }
 
 class PlayList {
-    constructor(playListName, listaCanciones, ordenEscucha){
+    constructor(playListName, listaCanciones){
         this.playListName = playListName;
         this.listaCanciones = listaCanciones;
-        this.ordenEscucha = ordenEscucha;
     }
     playPlayList() {
         this.listaCanciones.forEach(song => {
@@ -29,8 +29,48 @@ class PlayList {
     getPlayListName(){
         return this.playListName;
     }
+}
 
+class catalogoDeCanciones {
+    constructor(nombre, listaCanciones){
+        this.nombre = nombre;
+        this.listaCanciones = listaCanciones;
+    }
 
+    cargar(contenedor) {
+        this.listaCanciones.forEach(song => {
+
+            let elementoNombre = document.createElement("strong");
+            elementoNombre.innerText = song.nombre;
+
+            let botonPlay = document.createElement("img");
+            botonPlay.setAttribute("src", "./img/play-solid.svg");
+            botonPlay.setAttribute("width", "15");
+            botonPlay.setAttribute("height", "15");
+
+            let botonFavorite = document.createElement("img");
+            botonFavorite.setAttribute("src", "./img/heart-solid.svg");
+            botonFavorite.setAttribute("width", "15");
+            botonFavorite.setAttribute("height", "15");
+
+            let botonAdd = document.createElement("img");
+            botonAdd.setAttribute("src", "./img/plus-solid.svg");
+            botonAdd.setAttribute("width", "15");
+            botonAdd.setAttribute("height", "15");
+
+            let elementoLista = document.createElement("li");
+            elementoLista.appendChild(elementoNombre);
+            elementoLista.appendChild(botonPlay);
+            elementoLista.appendChild(botonFavorite);
+            elementoLista.appendChild(botonAdd);
+
+            contenedor.appendChild(elementoLista);
+        });
+    }
+
+    getPlayListName(){
+        return this.playListName;
+    }
 }
 
 class Reproductor {
@@ -38,9 +78,9 @@ class Reproductor {
     currentSong;
     constructor(){
         this.catalogoDeCanciones = [
-            new Song("cover1","nombre1","autor1","artista1","duracion1","album1","anio1","genero1","url1"),
-            new Song("cover2","nombre2","autor2","artista2","duracion2","album2","anio2","genero2","url2"),
-            new Song("cover3","nombre3","autor3","artista3","duracion3","album3","anio3","genero3","url3")
+            new Song(1,"cover1","nombre1","autor1","artista1","duracion1","album1","anio1","genero1","url1"),
+            new Song(2,"cover2","nombre2","autor2","artista2","duracion2","album2","anio2","genero2","url2"),
+            new Song(3,"cover3","nombre3","autor3","artista3","duracion3","album3","anio3","genero3","url3")
         ];
 
     }
@@ -89,3 +129,12 @@ class Reproductor {
 }
 
 let reproductor = new Reproductor();
+let listaCanciones = [
+    new Song(1,"cover1","Canción 1","autor1","artista1","duracion1","album1","anio1","genero1","url1"),
+    new Song(2,"cover2","Canción 2","autor2","artista2","duracion2","album2","anio2","genero2","url2"),
+    new Song(3,"cover3","Canción 3","autor3","artista3","duracion3","album3","anio3","genero3","url3")
+];
+
+let catalogo = new catalogoDeCanciones("", listaCanciones);
+let listaCatalogo = document.getElementById("catalogoDisponibleId");
+catalogo.cargar(listaCatalogo);
